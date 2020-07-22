6df05e617b435bf715028fda48e34136bf989860
Bas Westerbaan <bas@cloudflare.com>
2020-07-22 12:09:21 +0200

    circl: import crypto library CIRCL
    Imported from commit 574b5191de7809b1fe6da247c96175804029dcc3

---
+++ src/circl/pke/kyber/kyber.go
@@ -0,0 +1,10 @@
+//go:generate go run gen.go
+
+// Package kyber implements the CRYSTALS-Kyber.CPAPKE public key encrpyption
+// as submitted to round 3 of the NIST PQC competition and described in
+//
+// 	https://pq-crystals.org/kyber/data/kyber-specification-round3.pdf
+//
+// The related key encapsulation mechanism (KEM) CRYSTALS-Kyber.CCAKEM can
+// be found in the package circl/kem/kyber.
+package kyber
