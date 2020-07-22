c915434f6f53ebd95cbb34159de152a49330317f
Bas Westerbaan <bas@cloudflare.com>
2020-07-22 12:09:21 +0200

    circl: import crypto library CIRCL
    Imported from commit 574b5191de7809b1fe6da247c96175804029dcc3

---
+++ src/circl/dh/sidh/internal/common/utils.go
@@ -0,0 +1,50 @@
+package common
+
+import "encoding/binary"
+
+// Constant time select.
+// if pick == 1 (out = in1)
+// if pick == 0 (out = in2)
+// else out is undefined.
+func Cpick(pick int, out, in1, in2 []byte) {
+	var which = byte((int8(pick << 7)) >> 7)
+	for i := range out {
+		out[i] = (in1[i] & which) | (in2[i] & ^which)
+	}
+}
+
+// Read 2*bytelen(p) bytes into the given ExtensionFieldElement.
+//
+// It is an error to call this function if the input byte slice is less than 2*bytelen(p) bytes long.
+func BytesToFp2(fp2 *Fp2, input []byte, bytelen int) {
+	if len(input) < 2*bytelen {
+		panic("input byte slice too short")
+	}
+	numW64 := (bytelen*8 + 63) / 64
+	a := make([]byte, 8*numW64)
+	b := make([]byte, 8*numW64)
+	copy(a[:bytelen], input[:bytelen])
+	copy(b[:bytelen], input[bytelen:])
+	for i := 0; i < numW64; i++ {
+		fp2.A[i] = binary.LittleEndian.Uint64(a[i*8 : (i+1)*8])
+		fp2.B[i] = binary.LittleEndian.Uint64(b[i*8 : (i+1)*8])
+	}
+}
+
+// Convert the input to wire format.
+//
+// The output byte slice must be at least 2*bytelen(p) bytes long.
+func Fp2ToBytes(output []byte, fp2 *Fp2, bytelen int) {
+	if len(output) < 2*bytelen {
+		panic("output byte slice too short")
+	}
+	numW64 := (bytelen*8 + 63) / 64
+	a := make([]byte, 8*numW64)
+	b := make([]byte, 8*numW64)
+	for i := 0; i < numW64; i++ {
+		binary.LittleEndian.PutUint64(a[i*8:(i+1)*8], fp2.A[i])
+		binary.LittleEndian.PutUint64(b[i*8:(i+1)*8], fp2.B[i])
+	}
+	copy(output[:bytelen], a[:bytelen])
+	copy(output[bytelen:], b[:bytelen])
+}
---
+++ src/circl/ecc/p384/p384_test.go
@@ -0,0 +1,257 @@
+package p384_test
+
+import (
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"circl/ecc/p384"
+	"circl/internal/test"
+)
+
+func TestIsOnCurveTrue(t *testing.T) {
+	CirclCurve := p384.P384()
+	k := make([]byte, 384/8)
+	for i := 0; i < 128; i++ {
+		_, _ = rand.Read(k)
+		x, y := elliptic.P384().ScalarBaseMult(k)
+
+		got := CirclCurve.IsOnCurve(x, y)
+		want := true
+		if got != want {
+			test.ReportError(t, got, want, k)
+		}
+
+		x = x.Neg(x)
+		got = CirclCurve.IsOnCurve(x, y)
+		want = false
+		if got != want {
+			test.ReportError(t, got, want, k)
+		}
+	}
+}
+
+func TestAffine(t *testing.T) {
+	const testTimes = 1 << 7
+	CirclCurve := p384.P384()
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	t.Run("Addition", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			K1, _ := rand.Int(rand.Reader, params.N)
+			K2, _ := rand.Int(rand.Reader, params.N)
+			X1, Y1 := StdCurve.ScalarBaseMult(K1.Bytes())
+			X2, Y2 := StdCurve.ScalarBaseMult(K2.Bytes())
+			wantX, wantY := StdCurve.Add(X1, Y1, X2, Y2)
+			gotX, gotY := CirclCurve.Add(X1, Y1, X2, Y2)
+
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, K1, K2)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("Double", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			x, y := StdCurve.ScalarBaseMult(k.Bytes())
+			wantX, wantY := StdCurve.Double(x, y)
+
+			gotX, gotY := CirclCurve.Double(x, y)
+
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+}
+
+func TestScalarBaseMult(t *testing.T) {
+	const testTimes = 1 << 6
+	CirclCurve := p384.P384()
+	StdCurve := elliptic.P384()
+
+	t.Run("0P", func(t *testing.T) {
+		k := make([]byte, 500)
+		for i := 0; i < len(k); i += 20 {
+			gotX, gotY := CirclCurve.ScalarBaseMult(k[:i])
+			wantX, wantY := StdCurve.ScalarBaseMult(k[:i])
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k[:i])
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kP", func(t *testing.T) {
+		k := make([]byte, 48)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kSmall", func(t *testing.T) {
+		k := make([]byte, 16)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("kLarge", func(t *testing.T) {
+		k := make([]byte, 384)
+		for i := 0; i < testTimes; i++ {
+			_, _ = rand.Read(k)
+			gotX, gotY := CirclCurve.ScalarBaseMult(k)
+			wantX, wantY := StdCurve.ScalarBaseMult(k)
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+}
+
+func TestScalarMult(t *testing.T) {
+	const testTimes = 1 << 6
+	CirclCurve := p384.P384()
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	t.Run("k=0", func(t *testing.T) {
+		k := []byte{0x0}
+		gotX, gotY := CirclCurve.ScalarMult(params.Gx, params.Gy, k)
+		got := CirclCurve.IsAtInfinity(gotX, gotY)
+		want := true
+		if got != want {
+			test.ReportError(t, got, want)
+		}
+	})
+
+	t.Run("random k", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			gotX, gotY := CirclCurve.ScalarMult(params.Gx, params.Gy, k.Bytes())
+			wantX, wantY := StdCurve.ScalarMult(params.Gx, params.Gy, k.Bytes())
+
+			if gotX.Cmp(wantX) != 0 {
+				test.ReportError(t, gotX, wantX, k)
+			}
+			if gotY.Cmp(wantY) != 0 {
+				test.ReportError(t, gotY, wantY)
+			}
+		}
+	})
+
+	t.Run("wrong P", func(t *testing.T) {
+		for i := 0; i < testTimes; i++ {
+			k, _ := rand.Int(rand.Reader, params.N)
+			x, _ := rand.Int(rand.Reader, params.P)
+			y, _ := rand.Int(rand.Reader, params.P)
+
+			got := CirclCurve.IsOnCurve(CirclCurve.ScalarMult(x, y, k.Bytes()))
+			want := StdCurve.IsOnCurve(StdCurve.ScalarMult(x, y, k.Bytes()))
+
+			if got != want {
+				test.ReportError(t, got, want, k, x, y)
+			}
+		}
+	})
+}
+
+func TestCombinedMult(t *testing.T) {
+	const testTimes = 1 << 7
+	CirclCurve := p384.P384()
+	StdCurve := elliptic.P384()
+	params := StdCurve.Params()
+
+	for i := 0; i < testTimes; i++ {
+		K, _ := rand.Int(rand.Reader, params.N)
+		X, Y := StdCurve.ScalarBaseMult(K.Bytes())
+
+		K1, _ := rand.Int(rand.Reader, params.N)
+		K2, _ := rand.Int(rand.Reader, params.N)
+		x1, y1 := StdCurve.ScalarBaseMult(K1.Bytes())
+		x2, y2 := StdCurve.ScalarMult(X, Y, K2.Bytes())
+		wantX, wantY := StdCurve.Add(x1, y1, x2, y2)
+
+		gotX, gotY := CirclCurve.CombinedMult(X, Y, K1.Bytes(), K2.Bytes())
+		if gotX.Cmp(wantX) != 0 {
+			test.ReportError(t, gotX, wantX, K, K1, K2)
+		}
+		if gotY.Cmp(wantY) != 0 {
+			test.ReportError(t, gotY, wantY)
+		}
+	}
+}
+
+func BenchmarkScalarMult(b *testing.B) {
+	curve := p384.P384()
+	params := curve.Params()
+
+	K, _ := rand.Int(rand.Reader, params.N)
+	M, _ := rand.Int(rand.Reader, params.N)
+	N, _ := rand.Int(rand.Reader, params.N)
+	k := K.Bytes()
+	m := M.Bytes()
+	n := N.Bytes()
+
+	b.Run("kG", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			curve.ScalarBaseMult(k)
+		}
+	})
+	b.Run("kP", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			curve.ScalarMult(params.Gx, params.Gy, k)
+		}
+	})
+	b.Run("kG+lP", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_, _ = curve.CombinedMult(params.Gx, params.Gy, m, n)
+		}
+	})
+}
+
+func Example_p384() {
+	// import "circl/ecc/p384"
+	// import "crypto/elliptic"
+	circl := p384.P384()
+	stdlib := elliptic.P384()
+
+	params := circl.Params()
+	K, _ := rand.Int(rand.Reader, params.N)
+	k := K.Bytes()
+
+	x1, y1 := circl.ScalarBaseMult(k)
+	x2, y2 := stdlib.ScalarBaseMult(k)
+	fmt.Printf("%v, %v", x1.Cmp(x2) == 0, y1.Cmp(y2) == 0)
+	// Output: true, true
+}
