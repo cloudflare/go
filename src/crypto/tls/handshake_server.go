5b4de4f30cd33142c8d717c089b254e99d9967e1
Bas Westerbaan <bas@cloudflare.com>
2020-08-19 11:38:04 +0200

    x509/tls: add support for Circl signature schemes

--- src/crypto/tls/handshake_server.go
+++ src/crypto/tls/handshake_server.go
@@ -5,6 +5,8 @@
 package tls
 
 import (
+	circlSign "circl/sign"
+
 	"crypto"
 	"crypto/ecdsa"
 	"crypto/ed25519"
@@ -796,7 +798,7 @@ func (c *Conn) processCertsFromClient(certificate Certificate) error {
 
 	if len(certs) > 0 {
 		switch certs[0].PublicKey.(type) {
-		case *ecdsa.PublicKey, *rsa.PublicKey, ed25519.PublicKey:
+		case *ecdsa.PublicKey, *rsa.PublicKey, ed25519.PublicKey, circlSign.PublicKey:
 		default:
 			c.sendAlert(alertUnsupportedCertificate)
 			return fmt.Errorf("tls: client certificate contains an unsupported public key of type %T", certs[0].PublicKey)
