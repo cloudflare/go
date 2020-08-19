41259f66a24b2febfbca1b048e52b4b834ee3d7b
Sofía Celi <cherenkovd69@gmail.com>
2020-08-19 08:39:59 -0500

    crypto/tls: implement draft-ietf-tls-subcerts-10
    * Define API for delegated credentials so they are fetched using the
      same mechanisms used to fetch certificates
    * Allow the usage of other keyUsage when checking for the DC extension.
    * Add tool for generating delegated credentials.
    
    Co-authored-by: Jonathan Hoyland <jhoyland@cloudflare.com>

--- src/crypto/tls/handshake_client.go
+++ src/crypto/tls/handshake_client.go
@@ -169,6 +169,9 @@ func (c *Conn) makeClientHello() (*clientHelloMsg, clientKeySharePrivate, error)
 			hello.keyShares = []keyShare{{group: curveID, data: key.PublicKey().Bytes()}}
 			secret = key
 		}
+
+		hello.delegatedCredentialSupported = config.SupportDelegatedCredential
+		hello.supportedSignatureAlgorithmsDC = supportedSignatureAlgorithmsDC
 	}
 
 	if c.quic != nil {
@@ -1045,6 +1048,9 @@ func certificateRequestInfoFromMsg(ctx context.Context, vers uint16, certReq *ce
 		AcceptableCAs: certReq.certificateAuthorities,
 		Version:       vers,
 		ctx:           ctx,
+
+		SupportsDelegatedCredential: false, // Not supported in TLS <= 1.2
+		SignatureSchemesDC:          nil,   // Not supported in TLS <= 1.2
 	}
 
 	var rsaAvail, ecAvail bool
