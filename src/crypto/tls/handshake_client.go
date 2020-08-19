01466450a7f7d002a18c2a6753a370c124d19019
Bas Westerbaan <bas@cloudflare.com>
2020-08-19 11:38:04 +0200

    crypto: add support for CIRCL signature schemes
    To avoid having to regenerate all testdata files, add an option to
    control whether PQ signature algorithms are advertised. Tests were added
    for the client side.
    
    Since Go 1.19, FIPS-only mode must remain disabled to enable PQ sigalgs.
    
     [pwu: Go 1.17: moved parsePublicKey changes from x509/x509.go to x509/parser.go]
    
    Co-authored-by: Christopher Patton <chrispatton@gmail.com>
    Co-authored-by: Peter Wu <pwu@cloudflare.com>

--- src/crypto/tls/handshake_client.go
+++ src/crypto/tls/handshake_client.go
@@ -20,6 +20,8 @@ import (
 	"net"
 	"strings"
 	"time"
+
+	circlSign "github.com/cloudflare/circl/sign"
 )
 
 type clientHandshakeState struct {
@@ -118,7 +120,7 @@ func (c *Conn) makeClientHello() (*clientHelloMsg, clientKeySharePrivate, error)
 	}
 
 	if hello.vers >= VersionTLS12 {
-		hello.supportedSignatureAlgorithms = supportedSignatureAlgorithms()
+		hello.supportedSignatureAlgorithms = config.supportedSignatureAlgorithms()
 	}
 	if testingOnlyForceClientHelloSignatureAlgorithms != nil {
 		hello.supportedSignatureAlgorithms = testingOnlyForceClientHelloSignatureAlgorithms
@@ -922,7 +924,7 @@ func (c *Conn) verifyServerCertificate(certificates [][]byte) error {
 	}
 
 	switch certs[0].PublicKey.(type) {
-	case *rsa.PublicKey, *ecdsa.PublicKey, ed25519.PublicKey:
+	case *rsa.PublicKey, *ecdsa.PublicKey, ed25519.PublicKey, circlSign.PublicKey:
 		break
 	default:
 		c.sendAlert(alertUnsupportedCertificate)
