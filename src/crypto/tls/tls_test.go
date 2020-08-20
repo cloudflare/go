8946da6ec17c41733afe60689ce2e340d22fda2b
Sofía Celi <cherenkovd69@gmail.com>
2020-08-20 01:48:23 -0500

    Fix bug from migrating to 1.15

--- src/crypto/tls/tls_test.go
+++ src/crypto/tls/tls_test.go
@@ -790,7 +790,7 @@ func TestCloneNonFuncFields(t *testing.T) {
 		switch fn := typ.Field(i).Name; fn {
 		case "Rand":
 			f.Set(reflect.ValueOf(io.Reader(os.Stdin)))
-		case "Time", "GetCertificate", "GetConfigForClient", "VerifyPeerCertificate", "GetClientCertificate", "GetDelegatedCredential":
+		case "Time", "GetCertificate", "GetConfigForClient", "VerifyPeerCertificate", "VerifyConnection", "GetClientCertificate", "GetDelegatedCredential":
 			// DeepEqual can't compare functions. If you add a
 			// function field to this list, you must also change
 			// TestCloneFuncFields to ensure that the func field is
