8ddad8b49e5ffbed32a5d18514368f7c3315c391
Bas Westerbaan <bas@westerbaan.name>
2020-10-16 15:25:52 +0200

    Update circl

--- src/circl/sign/dilithium/mode4/internal/pack.go
+++ src/circl/sign/dilithium/mode4/internal/pack.go
@@ -1,3 +1,5 @@
+// Code generated from mode3/internal/pack.go by gen.go
+
 package internal
 
 import (
--- src/circl/sign/dilithium/mode4aes/internal/pack_test.go
+++ src/circl/sign/dilithium/mode4aes/internal/pack_test.go
@@ -1,3 +1,5 @@
+// Code generated from mode3/internal/pack_test.go by gen.go
+
 package internal
 
 import (
