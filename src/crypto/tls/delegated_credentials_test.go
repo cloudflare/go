c5c979acea1c063860a645f847e4714ab80a2c38
Thom Wiggers <thom@thomwiggers.nl>
2020-11-17 10:17:45 +0100

    Remove extra `initialize()` call

--- src/crypto/tls/delegated_credentials_test.go
+++ src/crypto/tls/delegated_credentials_test.go
@@ -403,7 +403,6 @@ func testConnWithDC(t *testing.T, clientMsg, serverMsg string, clientConfig, ser
 
 // Test the handshake with the delegated credential extension.
 func TestDCHandshake(t *testing.T) {
-	initialize()
 	serverMsg := "hello, client"
 	clientMsg := "hello, server"
 
