9d6c988ae4ecfcb5ced7eff7b13e8be663ffea84
Peter Wu <pwu@cloudflare.com>
2022-01-14 16:41:25 +0100

    crypto/tls: add PQSignatureSchemesEnabled option
    To avoid having to regenerate all testdata files, add an option to
    control whether PQ signature algorithms are advertised. Tests were added
    for the client side. The certificate was generated with:
    
        bin/go run src/crypto/tls/generate_cert.go -host example.golang -circl Ed25519-Dilithium3 -duration 87600h

--- src/crypto/tls/common.go
+++ src/crypto/tls/common.go
@@ -707,6 +707,11 @@ type Config struct {
 	// its key share in TLS 1.3. This may change in the future.
 	CurvePreferences []CurveID
 
+	// PQSignatureSchemesEnabled controls whether additional post-quantum
+	// signature schemes are supported for peer certificates. For available
+	// signature schemes, see tls_cf.go.
+	PQSignatureSchemesEnabled bool
+
 	// DynamicRecordSizingDisabled disables adaptive sizing of TLS records.
 	// When true, the largest possible TLS record size is always used. When
 	// false, the size of TLS records may be adjusted in an attempt to
@@ -811,6 +816,7 @@ func (c *Config) Clone() *Config {
 		MinVersion:                  c.MinVersion,
 		MaxVersion:                  c.MaxVersion,
 		CurvePreferences:            c.CurvePreferences,
+		PQSignatureSchemesEnabled:   c.PQSignatureSchemesEnabled,
 		DynamicRecordSizingDisabled: c.DynamicRecordSizingDisabled,
 		Renegotiation:               c.Renegotiation,
 		KeyLogWriter:                c.KeyLogWriter,
