789324b5cb46bc89c849c98f2b9194afa49576a9
Peter Wu <pwu@cloudflare.com>
2022-01-14 16:41:25 +0100

    crypto/tls: add PQSignatureSchemesEnabled option
    To avoid having to regenerate all testdata files, add an option to
    control whether PQ signature algorithms are advertised. Tests were added
    for the client side. The certificate was generated with:
    
        bin/go run src/crypto/tls/generate_cert.go -host example.golang -circl Ed25519-Dilithium3 -duration 87600h

--- src/crypto/tls/handshake_client_tls13.go
+++ src/crypto/tls/handshake_client_tls13.go
@@ -470,7 +470,7 @@ func (hs *clientHandshakeStateTLS13) readServerCertificate() error {
 	}
 
 	// See RFC 8446, Section 4.4.3.
-	if !isSupportedSignatureAlgorithm(certVerify.signatureAlgorithm, supportedSignatureAlgorithms) {
+	if !isSupportedSignatureAlgorithm(certVerify.signatureAlgorithm, c.config.supportedSignatureAlgorithms()) {
 		c.sendAlert(alertIllegalParameter)
 		return errors.New("tls: certificate used with invalid signature algorithm")
 	}
