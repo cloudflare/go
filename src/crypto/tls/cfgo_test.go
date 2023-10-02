1023c2ada2fc6b5572f530b253560d98ac2a3c01
Bas Westerbaan <bas@westerbaan.name>
2023-10-02 15:43:32 +0200

    Ensure that ClientCurveGuess is a subsequence of CurvePreferences

--- src/crypto/tls/cfgo_test.go
+++ src/crypto/tls/cfgo_test.go
@@ -5,5 +5,5 @@ package tls
 import "testing"
 
 func TestCfgoBuildTag(t *testing.T) {
-    t.Error("Build tag cfgo is expected to be set for this toolchain")
+	t.Error("Build tag cfgo is expected to be set for this toolchain")
 }
