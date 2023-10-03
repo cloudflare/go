4cdf9ea0d3cb8f7ebf9956ae56991815a5eae5b7
Bas Westerbaan <bas@cloudflare.com>
2023-10-03 13:24:35 +0200

    Use server's preferred key agreement
    In contrast to upstream Go, we will send an HelloRetryRequest and
    accept an extra roundtrip if there is a more preferred group, than
    the one the client has provided a keyshare for in the initial ClientHello.
    
    Cf. https://datatracker.ietf.org/doc/draft-davidben-tls-key-share-prediction/

--- src/crypto/tls/handshake_server_test.go
+++ src/crypto/tls/handshake_server_test.go
@@ -1934,6 +1934,7 @@ func TestAESCipherReorderingTLS13(t *testing.T) {
 					supportedVersions:  []uint16{VersionTLS13},
 					compressionMethods: []uint8{compressionNone},
 					keyShares:          []keyShare{{group: X25519, data: pk.PublicKey().Bytes()}},
+					supportedCurves:    []CurveID{X25519},
 				},
 			}
 
