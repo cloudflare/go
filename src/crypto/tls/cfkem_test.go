cf490a05dcf9533250dd459ef70342e285ecbe06
Bas Westerbaan <bas@cloudflare.com>
2023-10-10 13:26:07 +0200

    Add dummy key agreement with codepoint 0xfe33 (#162)
    DummyKex is a key agreeement similar in size but purposefully incompatible with
    X25519. The goal is to have a key agreement that servers will not support, so
    we can test HelloRetryRquest.

--- src/crypto/tls/cfkem_test.go
+++ src/crypto/tls/cfkem_test.go
@@ -95,6 +95,7 @@ func TestHybridKEX(t *testing.T) {
 		X25519Kyber768Draft00,
 		X25519Kyber768Draft00Old,
 		P256Kyber768Draft00,
+		DummyKex,
 	} {
 		run(curveID, true, true, false, false)
 		run(curveID, true, false, false, false)
